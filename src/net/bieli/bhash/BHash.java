010c9b4d690275cdc4f0407cb2e55cb070820628
Marcin Bielak <marcin.bieli@gmail.com>
2013-12-14 02:19:53 +0100

    remove comments

--- src/net/bieli/bhash/BHash.java
+++ src/net/bieli/bhash/BHash.java
@@ -12,16 +12,6 @@ public class BHash {
     private static int base_position = 65;
     private static ArrayList<Integer> positions = null;
 
-    /*
-            private static final int base_position = 65;Integer[] basePositions = {
-               842, 4, 9393, 134,
-               464, 35, 3884, 234,
-               24, 321, 3456, 734,
-               345, 2345, 34, 16,
-               4564, 3243, 234, 42,
-               2534, 63, 232, 234
-            };
-        */
     public String encode(String text)
     {
         if (positions == null) {
@@ -54,36 +44,11 @@ public class BHash {
     private String encodeAsciiText(ArrayList<Integer> positions, String text)
     {
         String hash = "";
-/*
-    public static void main(String[] args)
-            throws java.io.UnsupportedEncodingException {
-        String s = "Hola, señor!";
-        System.out.println(s);
-        byte[] b = new byte[s.length()];
-        for (int i = 0; i < b.length; i++) {
-            int cp = s.codePointAt(i);
-            b[i] = (byte) cp;
-            System.out.print((byte) cp + " ");
-        }
-*/
 
         for (Integer i : positions) {
             hash += text.charAt(i);
-            //hash += String.format("%02d", Character.toChars(i + 26));
-            //hash += Integer.toString(i + 32);
-            //basePositions
-            //hash += String.valueOf(i + 32);
             hash += (char) (i + base_position);
         }
-
-/*
-        byte [] key = "MYVERYINSECUREKEY".getBytes("ASCII");
-
-        for (int i = 0; i < text.length(); i++) {
-            int charAsInt = Character.getNumericValue(text.charAt(i));
-            list.add(charAsInt);
-        }
-*/
 
         return hash;
     }
@@ -117,4 +82,3 @@ public class BHash {
         this.base_position = base_position;
     }
 }
-
